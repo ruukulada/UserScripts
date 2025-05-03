3eb2a1ab4e8b4d78ca5b0efc628c39a1526c5563
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-05-03 01:33:48 -0400

    Create pwa-titlebar-light-dark-auto.user.js

---
+++ scripts/pwa-titlebar-light-dark-auto.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Auto Light/Dark PWA Titlebar
+// @version      1.0
+// @description  Changes titlebar of PWA to be light or dark
+// @author       https://github.com/ruukulada
+// @namespace    https://github.com/ruukulada
+// @match        *://*/*
+// @license      MIT
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+function toggleTheme(isDarkMode) {
+  'use strict';
+
+  var meta = document.createElement("meta");
+  meta.name = "theme-color";
+  const mode = isDarkMode ? "#000000" : "#FFFFFF";
+  meta.content = mode;
+  document.getElementsByTagName("head")[0].prepend(meta);
+}
+
+const darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
+toggleTheme(darkMedia.matches); // toggle theme when page loaded
+
+darkMedia.addEventListener('change', e => {
+  toggleTheme(e.matches); // toggle theme when system theme changed
+});
