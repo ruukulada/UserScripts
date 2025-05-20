dc7f23f36414111b792ad99731468e645cc74f1a
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-05-19 21:19:38 -0400

    Update twitter-no-slop-css.user.js

--- scripts/twitter-no-slop-css.user.js
+++ scripts/twitter-no-slop-css.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Twitter No Slop
+// @name         Twitter No Slop CSS
 // @version      1.0
 // @description  Twitter.
 // @author       https://github.com/ruukulada
