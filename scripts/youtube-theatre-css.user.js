0bd39e0563c2f2fe2c7db9c34f466ec41630f3ad
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-05-23 18:27:32 -0400

    Update youtube-theatre-css.user.js

--- scripts/youtube-theatre-css.user.js
+++ scripts/youtube-theatre-css.user.js
@@ -13,17 +13,34 @@
 
 (function () {
   'use strict';
+  function injectCSS(css) {
+    const style = document.createElement('style');
+    style.type = 'text/css'; style.innerHTML = css;
+    document.head.appendChild(style);
+  }
   function loadExternalCSS(url) {
     fetch(url)
-      .then((response) => {
-        return response.text();
-      })
-      .then((css) => {
-        const style = document.createElement('style');
-        style.type = 'text/css'; style.innerHTML = css;
-        document.head.appendChild(style);
-      })
+      .then((response) => response.text())
+      .then((css) => { injectCSS(css); })
   }
-  const cssUrl = 'https://ruukulada.github.io/UserStyles/src/youtubetheatre.css';
-  loadExternalCSS(cssUrl);
+  function applyStyles() {
+    const currentUrl = window.location.href;
+    if (currentUrl.includes('watch')) {
+      const videoPageCssUrl = 'https://ruukulada.github.io/UserStyles/src/youtubetheatre.css';
+      loadExternalCSS(videoPageCssUrl);
+    }
+    else {
+      const homePageCss = "div[id=masthead-container] { opacity: 100%; }";
+      injectCSS(homePageCss);
+    }
+  }
+  applyStyles();
+  let lastUrl = window.location.href;
+  new MutationObserver(() => {
+    const currentUrl = window.location.href;
+    if (currentUrl !== lastUrl) {
+      lastUrl = currentUrl;
+      applyStyles();
+    }
+  }).observe(document, { subtree: true, childList: true });
 })();
